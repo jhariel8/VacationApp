e30a757c104e26da7bceb90f98461d998df02b22
jhariel8 <jhariel8@gmail.com>
2020-05-31 13:44:01 -0500

    added cityinfo view

--- VacationApp/VacationApp/Views/StateInfoTemplatePage.xaml.cs
+++ VacationApp/VacationApp/Views/StateInfoTemplatePage.xaml.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using VacationApp.ViewModels;
+using VacationApp.Models;
 
 namespace VacationApp.Views
 {
@@ -18,6 +19,18 @@ namespace VacationApp.Views
             InitializeComponent();
 
             BindingContext = new StateInfoPageViewModel();
+        }
+
+        private void City1_Clicked(object sender, EventArgs e)
+        {
+            City1.BindingContext = new CityInfoViewModel()
+            {
+                DateImages = new List<DateImage>()
+                {
+                    new DateImage() { ImageSource = "uparrow.png", Date = "5/2/2020" },
+                    new DateImage() { ImageSource = "downarrow.png", Date="5/6/2020" }
+                }
+            };
         }
     }
 }
--- VacationApp/VacationApp/Views/cityinfoview.xaml.cs
+++ VacationApp/VacationApp/Views/cityinfoview.xaml.cs
@@ -10,9 +10,24 @@ using Xamarin.Forms.Xaml;
 namespace VacationApp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class View1 : ContentView
+    public partial class CityInfoView : ContentView
     {
-        public View1()
+        public static readonly BindableProperty CityNameProperty = BindableProperty.Create("CityName", typeof(string), typeof(CityInfoView), string.Empty);
+        public static readonly BindableProperty DescriptionProperty = BindableProperty.Create("Description", typeof(string), typeof(CityInfoView), "Scroll through the photos to see the changes in activity before and during the COVID-19 pandemic. Places with large spikes in activity should probably be avoided.");
+
+        public string CityName
+        {
+            get { return (string)GetValue(CityNameProperty); }
+            set { SetValue(CityNameProperty, value); }
+        }
+
+        public string Description
+        {
+            get { return (string)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+
+        public CityInfoView()
         {
             InitializeComponent();
         }
